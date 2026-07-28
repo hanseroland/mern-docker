cc56a91adb00366cd3d23a07e212513be2d8d9cc
hanseroland <rolandntougou@gmail.com>
2026-07-28 16:12:30 +0000

    chore: setting networking in Docker Ping Route

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -7,9 +7,17 @@ export const startServer = () => {
     const httpServer = express();
     const port = config.port;
 
+    // Test rout for port config
+
+    httpServer.get('/ping',(req,res)=>{
+        console.log(`Ping route : ${req.url} ${Date.now()}`);
+        res.status(200).json({
+            message:'Ping successful',
+        })
+    })
+
 
     try {
-
 
         httpServer.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
